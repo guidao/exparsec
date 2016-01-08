0fe75be6b35fd6a096be2bab1848ae5c92d940f2
guidao <964704149@qq.com>
2016-01-08 16:14:58 +0800

    添加组合，或者情况

--- lib/exparsec.ex
+++ lib/exparsec.ex
@@ -26,6 +26,30 @@ defmodule Exparsec do
     end
   end
 
+  def noneof(input) do
+    fn(x)->
+      !(x in input)
+    end
+  end
+
+  def char(input) do
+    input |> oneof |> return
+  end
+
+  def letter do
+    return(oneof('abcdefghijklmnopkrstuvwxyz'))
+  end
+
+  def symbol do
+    return(oneof '!$%&|*+ -/: <=? >@^_~#')
+  end
+
+  def digit do
+    return(oneof '0123456789')
+  end
+
+
+
   def skipMany(test) do
     {:parser, fn(state)->
       case state.input do
@@ -42,6 +66,55 @@ defmodule Exparsec do
     end}
   end
 
+  def many({:parser, _}=p) do
+    {:parser, fn(state)->
+      case runP(p, state) do
+        {:ok, val, nstate} ->
+          fix_return(val,runP(p, nstate))
+        error ->
+          error
+      end
+    end}
+  end
+
+  def many(test) do
+    {:parser, fn(state)->
+      case state.input do
+        [c|cs] ->
+          case test.(c) do
+            true ->
+              fix_return(c, runP(many(test), %State{state|input: cs}))
+            false ->
+              {:ok, [], state}
+          end
+        [] ->
+          {:ok, [], state}
+      end
+    end}
+  end
+
+  def many1(p) do
+    {:parser, fn(state)->
+      case runP(p, state) do
+        {:ok, val, nstate} ->
+          fix_return(val, runP(many(p), nstate))
+        error ->
+          error
+      end
+    end}
+  end
+
+
+  def fix_return([_|_] = c, {:ok, val, state}) do
+    {:ok, c ++ val, state}
+  end
+  def fix_return(c, {:ok, val, state}) do
+    {:ok, [c|val], state}
+  end
+  def fix_return(c, {:error, reason, state}) do
+    {:ok, [c], state}
+  end
+  
   def runP({:parser, f},%State{}=state) do
     f.(state)
   end
@@ -50,6 +123,7 @@ defmodule Exparsec do
     {:parser, fn(state)->
       case runP(p, state) do
         {:ok, val, nstate} ->
+          IO.puts "pval:#{inspect val}"
           runP(x, nstate)
         error ->
           error
@@ -58,6 +132,16 @@ defmodule Exparsec do
     end}
   end
 
+  def orelse(p, x) do
+    {:parser, fn(state)->
+      case runP(p, state) do
+        {:error, "not match", state} ->
+          runP(x, state)
+        ok ->
+          ok
+      end
+    end}
+  end
 
   def parse(p, name , input) do
     runP(p, initState(input))
