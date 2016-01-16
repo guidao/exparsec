edc2c8dbd85edb3e4c2ca4848605b0f4502101d3
guidao <964704149@qq.com>
2016-01-16 14:25:21 +0800

    fix

--- lib/exparsec_prim.ex
+++ lib/exparsec_prim.ex
@@ -1,13 +1,17 @@
 defmodule Exparsec.Prim do
   def oneof(input) do
     fn(x)->
-      x in input 
+      a = x in input 
+      #IO.puts "input:#{inspect input}, x:#{inspect x}, bool:#{inspect a}"
+      a
     end
   end
 
   def noneof(input) do
     fn(x)->
-      !(x in input)
+      a = !(x in input)
+      #IO.puts "input:#{inspect input}, x:#{inspect x}, bool:#{inspect a}"
+      a
     end
   end
 
